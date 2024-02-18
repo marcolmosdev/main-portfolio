70dbfb3382cc188927ee0fa7ea6ff1a8c2f7d11b
Marc <marcolmosdev@gmail.com>
2024-02-18 02:36:49 +0100

    fix: favicon changed

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { RouterProvider } from 'react-router-dom';
 import AppRouter from './app/AppRouter.tsx';
 import Background from './app/core/components/background/Background.tsx';
 
+// Detect preferred color scheme
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Change the favicon based on the color scheme
+const faviconLink = document.querySelector("link[rel*='icon']") as HTMLLinkElement;
+faviconLink.href = isDarkMode ? '/favicon-dark.ico' : '/favicon-light.ico';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Background/>
